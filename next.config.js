df8008f7c313c551fe09444dd51a995b621ae2a4
KBeDevel <theuserbit@proton.me>
2022-07-21 14:30:15 -0400

    fix: use webpack 4 for AWS Amplify compatibility
    Signed-off-by: KBeDevel <theuserbit@proton.me>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack5: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
